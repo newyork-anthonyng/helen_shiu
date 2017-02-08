a86f60bf71c154b388f4ea16713b7568c5d000ad
Anthony Ng <newyork.anthonyng@gmail.com>
2017-02-07 19:55:37 -0800

    Add prev/next functionality

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,22 @@
 class Router {
   constructor() {
     this.workContainerEl = Array.prototype.slice.apply(document.querySelectorAll('.work__container'));
+    this.prevNavEl = document.querySelector('.work__navigation__prev');
+    this.nextNavEl = document.querySelector('.work__navigation__next');
+    const links = Array.prototype.slice.apply(document.querySelectorAll('.work__grid a'));
+    this.linkHrefs = links.map((e) => {
+      return e.href.split('#')[1];
+    });
 
     this.addEventListeners = this.addEventListeners.bind(this);
     this.handleHashChange = this.handleHashChange.bind(this);
+    this.getCurrentHash = this.getCurrentHash.bind(this);
     this.getHashFromURL = this.getHashFromURL.bind(this);
     this.showWork = this.showWork.bind(this);
     this.hideAllWork = this.hideAllWork.bind(this);
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.getIndexOfCurrentHash = this.getIndexOfCurrentHash.bind(this);
 
     this.addEventListeners();
     this.handleHashChange();
@@ -14,16 +24,24 @@ class Router {
 
   addEventListeners() {
     window.addEventListener('hashchange', this.handleHashChange);
+
+    // add event listener to the navigation elements
+    this.prevNavEl.addEventListener('click', this.handlePrevClick);
+    this.nextNavEl.addEventListener('click', this.handleNextClick);
   }
 
   handleHashChange(e) {
-    const hash = this.getHashFromURL(window.location.hash);
+    const hash = this.getCurrentHash();
 
     if (!hash) {
       this.hideAllWork();
     } else {
       this.showWork(hash);
     }
+  }
+
+  getCurrentHash() {
+    return this.getHashFromURL(window.location.hash);
   }
 
   getHashFromURL(hash) {
@@ -45,6 +63,29 @@ class Router {
     this.workContainerEl.forEach(el => {
       el.classList.remove('work__container--active');
     });
+  }
+
+  handlePrevClick() {
+    const index = this.getIndexOfCurrentHash();
+
+    if (index <= 0) return;
+
+    const prevHash = this.linkHrefs[index - 1];
+    window.location.hash = prevHash;
+  }
+
+  getIndexOfCurrentHash() {
+    const currentHash = this.getCurrentHash();
+    return this.linkHrefs.indexOf(currentHash);
+  }
+
+  handleNextClick() {
+    const index = this.getIndexOfCurrentHash();
+
+    if (index >= this.linkHrefs.length - 1) return;
+
+    const nextHash = this.linkHrefs[index + 1];
+    window.location.hash = nextHash;
   }
 }
 
