757d18b67ebf4a382a30cc93f6bc23bb48ff06a1
Anthony Ng <newyork.anthonyng@gmail.com>
2017-02-10 22:25:15 -0800

    Handle /about page

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,9 @@ class Router {
     this.workContainerEl = Array.prototype.slice.apply(document.querySelectorAll('.work__container'));
     this.prevNavEl = document.querySelector('.work__navigation__prev');
     this.nextNavEl = document.querySelector('.work__navigation__next');
+    this.aboutEl = document.querySelector('.about');
+    this.workShowcaseEl = document.querySelector('.work__showcase');
+    this.thumbnailsEl = document.querySelector('.work__thumbnails');
     const links = Array.prototype.slice.apply(document.querySelectorAll('.work__thumbnails a'));
     this.linkHrefs = links.map((e) => {
       return e.href.split('#')[1];
@@ -13,10 +16,11 @@ class Router {
     this.getCurrentHash = this.getCurrentHash.bind(this);
     this.getHashFromURL = this.getHashFromURL.bind(this);
     this.showWork = this.showWork.bind(this);
-    this.hideAllWork = this.hideAllWork.bind(this);
     this.handlePrevClick = this.handlePrevClick.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
     this.getIndexOfCurrentHash = this.getIndexOfCurrentHash.bind(this);
+    this.showHomePage = this.showHomePage.bind(this);
+    this.showAboutPage = this.showAboutPage.bind(this);
 
     this.addEventListeners();
     this.handleHashChange();
@@ -33,8 +37,10 @@ class Router {
   handleHashChange(e) {
     const hash = this.getCurrentHash();
 
-    if (!hash) {
-      this.hideAllWork();
+    if (hash === 'about') {
+      this.showAboutPage();
+    } else if (!hash) {
+      this.showHomePage();
     } else {
       this.showWork(hash);
     }
@@ -49,19 +55,32 @@ class Router {
     return splitURL[splitURL.length - 1];
   }
 
+  showAboutPage() {
+    this.thumbnailsEl.classList.remove('work__thumbnails--active');
+    this.workShowcaseEl.classList.remove('work__showcase--active');
+
+    this.aboutEl.classList.add('about--active');
+  }
+
+  showHomePage() {
+    this.workShowcaseEl.classList.remove('work__showcase--active');
+    this.aboutEl.classList.remove('about--active');
+
+    this.thumbnailsEl.classList.add('work__thumbnails--active');
+  }
+
   showWork(name) {
+    this.aboutEl.classList.remove('about--active');
+    this.thumbnailsEl.classList.remove('work__thumbnails--active');
+
+    this.workShowcaseEl.classList.add('work__showcase--active');
+
     this.workContainerEl.forEach(el => {
       if (el.classList.contains(name)) {
         el.classList.add('work__container--active');
       } else {
         el.classList.remove('work__container--active');
       }
-    });
-  }
-
-  hideAllWork() {
-    this.workContainerEl.forEach(el => {
-      el.classList.remove('work__container--active');
     });
   }
 
