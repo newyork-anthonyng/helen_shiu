cf730236bb3b34e86135b186623a1ad54a6f5399
Anthony Ng <newyork.anthonyng@gmail.com>
2017-03-29 23:52:35 -0700

    Update styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 
+gulp.task('minify-js', () => {
+  return gulp.src('src/app.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('minify-html', () => {
   return gulp.src('src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    // .pipe(htmlmin())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', () => {
   return gulp.src('src/*.css')
-    .pipe(cleanCSS())
+    // .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
 });
