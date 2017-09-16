eee6dd59effe9bb439546a3d5cec41b344348709
Anthony Ng <newyork.anthonyng@gmail.com>
2017-09-16 07:23:25 -0700

    Add ReactRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './screens/App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('app')
 );
 
--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -1,127 +1,64 @@
 import React, { Component } from 'react';
 import Footer from 'components/Footer';
-import Menu from './components/Menu';
-import MenuButton from './components/MenuButton';
-import MenuDisplay from './components/MenuDisplay';
-import TargetBlankLink from './components/TargetBlankLink';
+import { Link } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router';
 import styles from './styles.css';
 
+const Home = () => (
+  <p>
+    I'm a NYC based graphic designer with a tax & accounting background, currently at RBX Active.
+  </p>
+);
+
+const Work = () => (
+  <div>
+    <a href="#">RBX Active</a>
+    <a href="/work/wanderer">Wanderer</a>
+    <a href="/work/swimtribe">Swim Tribe</a>
+    <a href="/work/nest">Nest</a>
+    <a href="/work/pooh">The Complete Tales of Winnie the Pooh</a>
+  </div>
+)
+
+const Contact = () => (
+  <div>
+    <a href="mailto:helen.shiu@outlook.com">Email</a>
+    <a href="#">Resume</a>
+    <a href="https://www.linkedin.com/in/helen-shiu-62384027">LinkedIn</a>
+  </div>
+);
+
+const NoMatch = () => (
+  <Redirect to="/" />
+);
+
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      url: '#'
-    };
+  componentDidMount() {
+    
   }
-
-  renderBody = () => {
-    switch(this.state.url) {
-      case '#work':
-        return this.renderWorkBody();
-      case '#contact':
-        return this.renderContactBody();
-      case '#':
-      default:
-        return this.renderHomeBody();
-    }
-  }
-
-  renderHomeBody() {
-    return (
-      <p>
-        I'm a NYC based graphic designer with a tax & accounting background, currently at RBX Active.
-      </p>
-    );
-  }
-
-  renderWorkBody() {
-    return (
-      <div>
-        <TargetBlankLink href="#">RBX Active</TargetBlankLink>
-        <TargetBlankLink href="/work/wanderer">Wanderer</TargetBlankLink>
-        <TargetBlankLink href="/work/swimtribe">Swim Tribe</TargetBlankLink>
-        <TargetBlankLink href="/work/nest">Nest</TargetBlankLink>
-        <TargetBlankLink href="/work/pooh">The Complete Tales of Winnie the Pooh</TargetBlankLink>
-      </div>
-    );
-  }
-
-  renderContactBody() {
-    return (
-      <div>
-        <TargetBlankLink href="mailto:helen.shiu@outlook.com">Email</TargetBlankLink>
-        <TargetBlankLink href="#">Resume</TargetBlankLink>
-        <TargetBlankLink href="https://www.linkedin.com/in/helen-shiu-62384027">LinkedIn</TargetBlankLink>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div>
-        <nav>
-          <a href="#" onClick={() => this.setState({ url: '#' })} >Helen Shiu</a>
-          <a href="#work" onClick={() => this.setState({ url: '#work' })}>Work</a>
-          <a href="#contact" onClick={() => this.setState({ url: '#contact' })}>Contact</a>
+        <nav className={styles.nav}>
+          <Link to="/">Helen Shiu</Link>
+
+          <div className={styles.secondaryNav}>
+            <Link to="/work">Work</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
         </nav>
 
-        {this.renderBody()}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/work" component={Work} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
 
         <Footer />
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   renderHeading = () => {
-//     return (
-//       <header>
-//         <h1>Helen Shiu</h1>
-// 
-//         <Menu>
-//           <p className={styles.inlineText}>
-//             I am a NYC based graphic designer, currently at RBX Active.
-//             If you'd like to see more work, collaborate, or say hello,
-//           </p>
-//           <MenuButton><span className={styles.menuButton}>&nbsp;let's stay in touch!</span></MenuButton>
-// 
-//           <MenuDisplay className={styles.headingMenuDisplay}>
-//             <TargetBlankLink href="mailto:helen.shiu@outlook.com">Email</TargetBlankLink>
-//             <TargetBlankLink href="#">Resume</TargetBlankLink>
-//             <TargetBlankLink href="https://www.linkedin.com/in/helen-shiu-62384027">LinkedIn</TargetBlankLink>
-//           </MenuDisplay>
-//         </Menu>
-//       </header>
-//     );
-//   }
-// 
-//   renderWork = () => {
-//     return (
-//       <Menu>
-//         <MenuButton><h2 className={styles.menuButton}>Work</h2></MenuButton>
-//         <MenuDisplay className={styles.workMenuDisplay}>
-//           <TargetBlankLink href="#">RBX Active</TargetBlankLink>
-//           <TargetBlankLink href="/work/wanderer">Wanderer</TargetBlankLink>
-//           <TargetBlankLink href="/work/swimtribe">Swim Tribe</TargetBlankLink>
-//           <TargetBlankLink href="/work/nest">Nest</TargetBlankLink>
-//           <TargetBlankLink href="/work/pooh">The Complete Tales of Winnie the Pooh</TargetBlankLink>
-//         </MenuDisplay>
-//       </Menu>
-//     );
-//   }
-// 
-//   render() {
-//     return (
-//       <div>
-//         {this.renderHeading()}
-//         {this.renderWork()}
-// 
-//         <Footer className={styles.footer} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
